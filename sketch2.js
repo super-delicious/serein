87c27a15eb15f65345c24cbcefd23b6f3989175c
super-delicious <61995976+super-delicious@users.noreply.github.com>
2021-04-17 17:48:36 -0700

    updated nav search

--- sketch2.js
+++
@@ -1,9 +0,0 @@
-var $menu = $('#menu'),
-    $search = $( "#search" ),
-    $search_box = $(".search_box");
-
-//SEARCH BOX ADDS CLASS
-  $search.click(function(e) {
-		e.preventDefault();
-	$search_box.toggleClass('active');
-	});
